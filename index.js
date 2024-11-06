ae5c14148a54864021c921d7de8dc51f8a2b2ca0
Ayoub-Mabrouk <ayoubmabroukcontact@gmail.com>
2024-11-06 02:39:36 +0100

    Refactor collapseLeadingSlashes function for simplicity and readability
    Replaced the old implementation of collapseLeadingSlashes, which iterated over each character, with a more concise and efficient version using a regular expression. The new version uses eplace() to collapse leading slashes into a single slash and handles empty strings by returning '/'. This update improves code readability and leverages modern JavaScript string methods.

--- index.js
+++ index.js
@@ -129,16 +129,9 @@ function serveStatic (root, options) {
  * Collapse all leading slashes into a single slash
  * @private
  */
-function collapseLeadingSlashes (str) {
-  for (var i = 0; i < str.length; i++) {
-    if (str.charCodeAt(i) !== 0x2f /* / */) {
-      break
-    }
-  }
 
-  return i > 1
-    ? '/' + str.substr(i)
-    : str
+function collapseLeadingSlashes (str) {
+  return str.replace(/^\/+/, '/') || '/'
 }
 
 /**
